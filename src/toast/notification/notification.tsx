8ef23bacef6decc922122dd01625d19adf51cd01
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-09-30 23:58:31 +0530

    add css types

--- src/toast/notification/notification.tsx
+++ src/toast/notification/notification.tsx
@@ -4,19 +4,42 @@ import Danger from '../../assets/danger.jpeg'
 import Success from '../../assets/success.png'
 
 
-export default function Notification() {
+
+const NotificationTypes ={
+  danger : Danger,
+  success :Success,
+  info : Alert,
+}
+
+type NotificationProp={
+  position : "top-right"|"top-right"|"bottom-left"|"bottom-right";
+  type : "danger"|"info"|"success";
+  title: string;
+  desc:string;
+  cta :string;
+  onRemove:()=>{},
+  id:string
+}
+
+export default function Notification({title = "",desc ="", cta = "", id = "",type ="" ,onRemove=()=>{}}:NotificationProp) {
+
+  function handleRemove(){
+    onRemove(id);
+  }
   return (
-    <div className="toast">
+    <div className="toast"  data-types={NotificationTypes[type]}  data-position={}>
         <button className='toast-close'>&times;</button>
         <div className="toast-content">
             <div className="toast-info">
-                <img src={Alert}  height={"45px"} width={"45px"}/>
+                <img src={NotificationTypes[type]}  height={"45px"} width={"45px"}/>
                 <div className="toast-title-desc">
-                    <span>Title</span>
-                    <span>Description</span>
+                    <span>{title}</span>
+                    <span>{desc}</span>
                 </div>
             </div>
-            <div className="toast-cta"></div>
+            <div className="toast-cta">
+              {cta}
+            </div>
         </div>
         <div className="toast-progress"></div>
     </div>
