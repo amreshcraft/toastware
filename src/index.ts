24bef3e9ead5207eb9e1ef4045b5291ae652af6b
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-10-05 15:26:51 +0530

    publish v1.0.5 , fix issues

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-// index.jsx
+
+import './toast/toast-ui/style.css'
+import './toast/toast-container/style.css'
+
 export { default as ToastProvider } from './toast/toast-provider/ToastProvider';
 export { useToast } from './toast/hook/useToast'
 export { default as Toastware } from './toast/toast-ui/Toast';
+export * from './toast/types/types'
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ insertTypesEntry: true, outDir: 'dist' ,    }),
+    cssInjectedByJsPlugin()
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'Toastware',
-      fileName: (format) => `toastware.${format}.js`
+      fileName: format => `toastware.${format}.js`,
+      formats:["cjs","es","umd"]
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        },
-        exports: 'named'  // <- add this
-      }
+        globals: { react: 'React', 'react-dom': 'ReactDOM' },
+        exports: 'named',
+      },
     },
-  }
-})
+    cssCodeSplit: true,
+    minify: 'esbuild',
+    sourcemap: true,
+  },
+});
