42be53e36acb8993e3385f42af296665370baa4e
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-10-05 17:29:15 +0530

    publish v1.0.6 , fix type issue

---
+++ src/toast/types/css.d.ts
@@ -0,0 +1,2 @@
+// declarations.d.ts (ya src/css.d.ts)
+declare module '*.css';
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,41 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
-import path from 'path';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({ insertTypesEntry: true, outDir: 'dist' ,    }),
-    cssInjectedByJsPlugin()
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+      tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+      include: ['src/**/*', 'src/toast/types/css.d.ts', 'src/toast/types/assets.d.ts', 'src/toast/types/types.ts'],
+      exclude: ['node_modules', 'dist','src/App.tsx','src/main.tsx','src/App.css','src/index.css'],
+      outDir: 'dist',
+    }),
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'Toastware',
-      fileName: format => `toastware.${format}.js`,
-      formats:["cjs","es","umd"]
+      fileName: (format) => `toastware.${format}.js`,
+      formats: ['es', 'umd', 'cjs'],
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
-        globals: { react: 'React', 'react-dom': 'ReactDOM' },
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
         exports: 'named',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'style.css';
+          }
+          return '[name].[ext]'; // Always return a string
+        },
       },
     },
     cssCodeSplit: true,
