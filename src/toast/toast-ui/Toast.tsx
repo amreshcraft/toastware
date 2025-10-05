2cba9dc99a248167d54e1fc5738e5ef2a9f60f37
Amresh Maurya <210040598+amreshcraft@users.noreply.github.com>
2025-10-05 22:57:35 +0530

    minified package : change static image to react icon , use lazy loading

--- src/toast/toast-ui/Toast.tsx
+++ src/toast/toast-ui/Toast.tsx
@@ -1,19 +1,36 @@
 import './style.css'
 import { useToast } from '../hook/useToast'
 import type { ToastItem } from '../types/types'
-import { useEffect, useRef, useState } from 'react'
-import Info from '../../assets/info.png'
-import Success from '../../assets/success.png'
-import Warning from "../../assets/warning.png"
-import ErrorIcon from "../../assets/error.png"
-import Close from '../../assets/close.png'
+import { Suspense, useEffect, useRef, useState } from 'react'
+
+import React from 'react'
 
 
-const ToastIcons: Record<"info" | "success" | "warning" | "error", string> = {
-  info: Info,
-  success: Success,
-  warning: Warning,
-  error: ErrorIcon,
+import type { IconType } from "react-icons";
+
+const LazyInfo = React.lazy(() =>
+  import("react-icons/fa").then(mod => ({ default: mod.FaInfoCircle as IconType }))
+);
+const LazySuccess = React.lazy(() =>
+  import("react-icons/si").then(mod => ({ default: mod.SiTicktick as IconType }))
+);
+const LazyWarning = React.lazy(() =>
+  import("react-icons/io5").then(mod => ({ default: mod.IoWarning as IconType }))
+);
+const LazyError = React.lazy(() =>
+  import("react-icons/md").then(mod => ({ default: mod.MdOutlineError as IconType }))
+);
+const LazyClose = React.lazy(() =>
+  import("react-icons/io5").then(mod => ({ default: mod.IoCloseCircle as IconType }))
+);
+
+
+
+const ToastIcons: Record<"info" | "success" | "warning" | "error", React.LazyExoticComponent<IconType>> = {
+  info: LazyInfo,
+  success: LazySuccess,
+  warning: LazyWarning,
+  error: LazyError,
 };
 
 export default function Toast({ id, message, type = "info", position,duration = 5000 }: ToastItem) {
@@ -61,7 +78,7 @@ useEffect(() => {
     startTimeRef.current = Date.now();
   };
 
-
+ const Icon = ToastIcons[type];
 
   return (
     <div className={`toast ${position}`} data-toast-type={type}
@@ -69,11 +86,15 @@ useEffect(() => {
       onMouseLeave={handleResume}
       >
       <div className="toast-content">
-        <img src={ToastIcons[type]} width={45} height={45} alt={type} />
+        {/* <img src={ToastIcons[type]} width={45} height={45} alt={type} /> */}
+           <Suspense fallback={<span>...</span>}>
+          <Icon size={45} />
+        </Suspense>
         <span>{message}</span>
       </div>
       <button onClick={() => removeToast(id)} className="toast-close">
-        <img src={Close} width={30} height={30} alt="close" />
+        {/* <img src={<LazyClose/>} width={30} height={30} alt="close" /> */}
+        <LazyClose color='red' size={30}/>
       </button>
 
       <div className="toast-progress">
